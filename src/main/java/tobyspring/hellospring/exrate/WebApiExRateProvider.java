cd48b35f7252eac2372214ac8a6a48663e1eb0ef
Lavelee954 <darkhorse954@naver.com>
2024-07-28 12:20:34 +0900

    디폴트 콜백과 템플릿 빈

--- src/main/java/tobyspring/hellospring/exrate/WebApiExRateProvider.java
+++ src/main/java/tobyspring/hellospring/exrate/WebApiExRateProvider.java
@@ -6,12 +6,16 @@ import tobyspring.hellospring.payment.ExRateProvider;
 import java.math.BigDecimal;
 
 public class WebApiExRateProvider implements ExRateProvider {
-    ApiTemplate apiTemplate = new ApiTemplate();
+    private final ApiTemplate apiTemplate;
+
+    public WebApiExRateProvider(ApiTemplate apiTemplate) {
+        this.apiTemplate = apiTemplate;
+    }
 
     @Override
     public BigDecimal getExRate(String currency) {
         String url = "https://open.er-api.com/v6/latest/" + currency;
 
-        return apiTemplate.getExRate(url, new HttpClinetApiExercutor(), new ErApiExtractor());
+        return apiTemplate.getForExRate(url);
     }
 }
